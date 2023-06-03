767d8c150a9cab439c48ed97ba733ac09568b6ad
Luke Orth <lorth001@gmail.com>
2023-06-03 05:04:54 -0500

    Fixed some issues in the theme

--- public/js/light_dark.js
+++ public/js/light_dark.js
@@ -18,15 +18,22 @@ if (currentTheme == "dark") {
 
 btn.addEventListener("click", function () {
     document.body.classList.toggle("dark-theme");
-
+    let hasComments = document.getElementsByClassName("comments")[0];
     let theme = "light";
+
     if (document.body.classList.contains("dark-theme")) {
         theme = "dark";
         moon.style.display = 'none';
         sun.style.display = 'block';
+        if (hasComments) {
+            window.REMARK42.changeTheme("dark");
+        }
     } else {
         moon.style.display = 'block';
         sun.style.display = 'none';
+        if (hasComments) {
+            window.REMARK42.changeTheme("light");
+        }
     }
     localStorage.setItem("theme", theme);
 });
